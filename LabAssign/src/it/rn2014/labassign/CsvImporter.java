9f8c83b41d242c1fbe2f45be3c5215012e95b09e
Nicola Corti <corti.nico@gmail.com>
2014-06-05 01:24:21 +0200

    Refactored CsvImporter Code

--- LabAssign/src/it/rn2014/labassign/CsvImporter.java
+++ LabAssign/src/it/rn2014/labassign/CsvImporter.java
@@ -7,17 +7,27 @@ import java.io.IOException;
 
 public class CsvImporter {
 
-	public static void insertConstraintCodici(MySqlConnector conn, RoverList rl) {
-		try {
+	/**
+	 * Funzione che legge dal file "codici.csv" l'elenco dei ragazzi che parteciperanno al laboratorio CODICI
+	 * e li inserisce nel DB come vincoli
+	 * 
+	 * @param conn Connettore MySQL al DB di destinazione
+	 * @param rl Elenco di rover (per verificare se esistono)
+	 * @throws Exception In caso di funzionamento errato di I/O
+	 */
+	public static void insertConstraintCodici(MySqlConnector conn, RoverList rl) throws Exception {
+		
 			BufferedReader f = new BufferedReader(new FileReader("codici.csv"));
 			String line = f.readLine();
 			
 			while((line = f.readLine()) != null){
 				String[] sep = line.split(";");
+				
+				// Ricerco se il ragazzo e' presente nel DB
 				boolean found = false;
-				for(Rover r: rl){
+				for(Rover r: rl)
 					if (Double.parseDouble(sep[2]) == r.getCode()) found = true;
-				}
+				
 				if (!found) {
 					System.err.println("RAGAZZO " + sep[2]);
 				} else {
@@ -25,71 +35,58 @@ public class CsvImporter {
 				}
 			}
 			f.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	
 
-	public static void insertConstraintLabRS(MySqlConnector conn, RoverList rl) {
-		try {
+	public static void insertConstraintLabRS(MySqlConnector conn, RoverList rl) throws Exception {
+	
+		
 			BufferedReader f = new BufferedReader(new FileReader("lab-rs.csv"));
 			String line = f.readLine();
-			int s = 0;
+			
 			while((line = f.readLine()) != null){
 				String[] sep = line.split(";");
+				
+				// Escludo i capi dall'inserimento
 				if (sep[0].contains("capo")) continue;
+				
+				// Ricerco i ragazzi
 				boolean found = false;
-				for(Rover r: rl){
+				for(Rover r: rl)
 					if (Double.parseDouble(sep[3]) == r.getCode()) found = true;
-				}
+				
 				if (!found) {
 					System.err.println("ASSENTE: " + sep[3] + " - " + sep[1] + ", " + sep[2] + ", " + sep[4]);
-					s++;
-				} else {
-					if (conn.executeCount("SELECT * FROM vincoli WHERE codicecensimento = '" + sep[3] + "'") > 0){
-						System.err.println("PROBLEMA " + sep[3] + " - " + sep[1] + ", " + sep[2] + ", " + sep[4]);
-						String result = null;
-						if (sep[7].contains("1")){
-							result = conn.executeString("SELECT turn1 FROM vincoli 	WHERE codicecensimento = '" + sep[3] + "'");
-							if (result == null)
-								System.err.println("C'E POSTO");
-							else
-								System.err.println("NON C'E POSTO");
-						}
-						if (sep[7].contains("2")){
-							result = conn.executeString("SELECT turn2 FROM vincoli 	WHERE codicecensimento = '" + sep[3] + "'");
-							if (result == null)
-								System.err.println("C'E POSTO");
-							else
-								System.err.println("NON C'E POSTO");
-						}
-						if (sep[7].contains("3")){
-							result = conn.executeString("SELECT turn3 FROM vincoli 	WHERE codicecensimento = '" + sep[3] + "'");
-							if (result == null)
-								System.err.println("C'E POSTO");
-							else
-								System.err.println("NON C'E POSTO");
-						}
-					
-					}
-					/*else {
-						if (sep[7].contains("1"))
-							conn.update("INSERT INTO vincoli (codicecensimento, turn1) VALUES ('" + sep[3] + "','LAB-RS-TEMP-" + sep[6] + "', NULL, NULL)");
-						if (sep[7].contains("2"))
-							conn.update("INSERT INTO vincoli (codicecensimento, turn1) VALUES (NULL,'" + sep[3] + "','LAB-RS-TEMP-" + sep[6] + "', NULL)");
-						if (sep[7].contains("3"))
-							conn.update("INSERT INTO vincoli (codicecensimento, turn1) VALUES (NULL, NULL,'" + sep[3] + "','LAB-RS-TEMP-" + sep[6] + "')");
-						
-					}*/
+					continue;
+				} 
+				
+				// Controllo inconsistenza se e' presente un altro vincolo
+				if (conn.executeCount("SELECT * FROM vincoli WHERE codicecensimento = '" + sep[3] + "'") > 0){		
+					String result = conn.executeString("SELECT turn" + sep[7] + " FROM vincoli 	WHERE codicecensimento = '" + sep[3] + "'");
+					if (result != null) System.err.println("PROBLEMA: " + sep[3] + " - " + sep[1] + ", " + sep[2] + ", " + sep[4]);
 				}
+				
+				if (sep[7].contains("1"))
+					conn.update("INSERT INTO vincoli (codicecensimento, turn1, turn2, turn3) VALUES ('" + sep[3] + "','TEMP-RS-LAB-?-?-" + sep[5] + "', NULL, NULL)");
+				if (sep[7].contains("2"))
+					conn.update("INSERT INTO vincoli (codicecensimento, turn1, turn2, turn3) VALUES ('" + sep[3] + "', NULL, 'TEMP-RS-LAB-?-?-" + sep[5] + "', NULL)");
+				if (sep[7].contains("3"))
+					conn.update("INSERT INTO vincoli (codicecensimento, turn1, turn2, turn3) VALUES ('" + sep[3] + "', NULL, NULL,'TEMP-RS-LAB-?-?-" + sep[5] + "')");
 			}
-			System.err.println("TOTALE ERRORI " + s);
+
+			f.close();
+			
+	} 
+	
+	public static void insertLabs(MySqlConnector conn) {
+		try {
+			BufferedReader f = new BufferedReader(new FileReader("laboratori.csv"));
+			String line = f.readLine();
+			
+			while((line = f.readLine()) != null){
+				String[] sep = line.split(";");
+				System.out.println(sep);
+			}
 			f.close();
 			
 		} catch (FileNotFoundException e) {
@@ -100,7 +97,6 @@ public class CsvImporter {
 			e.printStackTrace();
 		}
 	}
-	
 	
 	
 }
